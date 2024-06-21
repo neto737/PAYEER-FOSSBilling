691f6c594f7f78374ec236e6373720e31b4fce4a
neto737 <tynhomello@gmail.com>
2024-06-21 22:36:19 +0000

    First commit

---
+++ PAYEER.php
@@ -0,0 +1,189 @@
+<?php
+
+class Payment_Adapter_PAYEER implements \FOSSBilling\InjectionAwareInterface
+{
+    protected ?Pimple\Container $di = null;
+    private array $config = [];
+
+    public function setDi(Pimple\Container $di): void
+    {
+        $this->di = $di;
+    }
+
+    public function getDi(): ?Pimple\Container
+    {
+        return $this->di;
+    }
+
+    public function __construct(array $config)
+    {
+        $this->config = $config;
+        foreach (['merchant_id', 'secret_key'] as $key) {
+            if (!isset($this->config[$key])) {
+                throw new \Payment_Exception('The ":pay_gateway" payment gateway is not fully configured. Please configure the :missing', [':pay_gateway' => 'BTCPay', ':missing' => $key], 4001);
+            }
+        }
+    }
+
+    public static function getConfig(): array
+    {
+        return [
+            'supports_one_time_payments' => true,
+            'description'                => 'PAYEER Payment Gateway',
+            'logo'                       => [
+                'logo'   => '/PAYEER/payeer-logo.png',
+                'height' => '50px',
+                'width'  => '50px',
+            ],
+            'form'              => [
+                'merchant_id'   => [
+                    'text',
+                    [
+                        'label' => 'Merchant ID:',
+                    ],
+                ],
+                'secret_key'    => [
+                    'text',
+                    [
+                        'label' => 'Secret key:',
+                    ],
+                ]
+            ]
+        ];
+    }
+
+    public function getHtml($api_admin, $invoice_id): string
+    {
+        $invoiceModel = $this->di['db']->load('Invoice', $invoice_id);
+
+        return $this->_generateForm($invoiceModel);
+    }
+
+    public function processTransaction($api_admin, $id, $data, $gateway_id): void
+    {
+        $post = $data['post'];
+
+        if ($post['m_operation_id'] && $post['m_sign']) {
+            $arHash = [
+                $post['m_operation_id'],
+                $post['m_operation_ps'],
+                $post['m_operation_date'],
+                $post['m_operation_pay_date'],
+                $post['m_shop'],
+                $post['m_orderid'],
+                $post['m_amount'],
+                $post['m_curr'],
+                $post['m_desc'],
+                $post['m_status']
+            ];
+
+            if (isset($post['m_params'])) {
+                $arHash[] = $post['m_params'];
+            }
+
+            $arHash[] = $this->config['secret_key'];
+
+            $sign_hash = strtoupper(hash('sha256', implode(':', $arHash)));
+
+            if ($this->isIpnDuplicate($post)) {
+                echo $post['m_orderid'] . '|success';
+                return;
+            }
+
+            if ($post['m_sign'] === $sign_hash && $post['m_status'] === 'success') {
+                $invoice = $this->di['db']->getExistingModelById('Invoice', $post['m_orderid']);
+
+                $transaction = $this->di['db']->dispense('Transaction');
+                $transaction->invoice_id = $invoice->id;
+                $transaction->gateway_id = $invoice->gateway_id;
+                $transaction->txn_id = $post['m_operation_id'];
+                $transaction->txn_status = $post['m_status'];
+                $transaction->amount = $post['m_amount'];
+                $transaction->currency = $post['m_curr'];
+
+                $bd = [
+                    'amount' => $transaction->amount,
+                    'description' => 'PAYEER transaction ' . $post['m_operation_id'],
+                    'type' => 'transaction',
+                    'rel_id' => $transaction->id,
+                ];
+
+                $client = $this->di['db']->getExistingModelById('Client', $invoice->client_id);
+                $clientService = $this->di['mod_service']('client');
+                $clientService->addFunds($client, $bd['amount'], $bd['description'], $bd);
+
+                $invoiceService = $this->di['mod_service']('Invoice');
+                if ($transaction->invoice_id) {
+                    $invoiceService->payInvoiceWithCredits($invoice);
+                }
+                $invoiceService->doBatchPayWithCredits(['client_id' => $client->id]);
+
+                $transaction->status = $post['m_status'];
+                $transaction->ipn = json_encode($post);
+                $transaction->updated_at = date('Y-m-d H:i:s');
+                $this->di['db']->store($transaction);
+
+                echo $post['m_orderid'] . '|success';
+                return;
+            }
+
+            echo $post['m_orderid'] . '|error';
+            return;
+        }
+    }
+
+    protected function _generateForm(Model_Invoice $invoice): string
+    {
+        $invoiceService = $this->di['mod_service']('Invoice');
+
+        $invoice_number_padding = $this->di['mod_service']('System')->getParamValue('invoice_number_padding');
+        $invoice_number_padding = $invoice_number_padding !== null && $invoice_number_padding !== '' ? $invoice_number_padding : 5;
+
+        $payeer['m_shop'] = $this->config['merchant_id'];
+        $payeer['m_orderid'] = $invoice->id;
+        $payeer['m_amount'] = $invoiceService->getTotalWithTax($invoice);
+        $payeer['m_curr'] = 'USD';
+        $payeer['m_desc'] = base64_encode('Order #' . $invoice->serie . sprintf('%0' . $invoice_number_padding . 's', $invoice->nr));
+        $payeer['m_key'] = $this->config['secret_key'];
+
+        $arHash = [
+            $payeer['m_shop'],
+            $payeer['m_orderid'],
+            $payeer['m_amount'],
+            $payeer['m_curr'],
+            $payeer['m_desc'],
+            $payeer['m_key']
+        ];
+
+        $payeer['m_sign'] = strtoupper(hash('sha256', implode(':', $arHash)));
+        $payeer['m_process'] = 'send';
+
+        // Remove key before building HTTP query
+        unset($payeer['m_key']);
+
+        $url = sprintf('https://payeer.com/merchant/?%s', http_build_query($payeer));
+
+        return '<script type="text/javascript">window.location = "' . $url . '";</script>';
+    }
+
+    public function isIpnDuplicate(array $ipn): bool
+    {
+        $sql = 'SELECT id
+                FROM transaction
+                WHERE txn_id = :transaction_id
+                  AND amount = :transaction_amount
+                LIMIT 2';
+
+        $bindings = [
+            ':transaction_id' => $ipn['m_operation_id'],
+            ':transaction_amount' => $ipn['m_amount'],
+        ];
+
+        $rows = $this->di['db']->getAll($sql, $bindings);
+        if ((is_countable($rows) ? count($rows) : 0) > 1) {
+            return true;
+        }
+
+        return false;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# PAYEER-FOSSBilling
-PAYEER extensions for FOSSBilling
+# PAYEER for FOSSBilling
+Quickly and easily integrate [PAYEER](https://payeer.com) into your [FOSSBilling](https://fossbilling.org) instance using this extension.
+
+## Installation
+
+### Manual installation
+1. Download the latest release from the [releases](https://github.com/neto737/PAYEER-FOSSBilling/releases/latest)
+2. Create a new folder named `PAYEER` in the `/library/Payment/Adapter` directory of your FOSSBilling installation
+3. Extract the archive you've downloaded in the first step into the new directory
+4. Go to the "Payment gateways" page in your admin panel (under the "System" menu in the navigation bar) and find PAYEER in the "New payment gateway" tab
+5. Click the cog icon next to PAYEER to install and configure PAYEER
+
+## Licensing
+This extension is licensed under the MIT license. See the [LICENSE](LICENSE) file for more information.
+
+## Disclaimer
+This extension is not affiliated with PAYEER Limited. in any way. PAYEER is a registered trademark of PAYEER Limited.
